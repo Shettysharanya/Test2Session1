7a97f3b81cc208b055960eb73bbb2a5b3816f9e5
sharanya0709 <94777111+Shettysharanya@users.noreply.github.com>
2022-02-28 06:45:06 +0000

    p2original.c

--- p2original.c
+++ p2original.c
@@ -1,42 +1,30 @@
-#include<stdio.h>
-int input()
-{
-  int n;
-  printf("enter the number\n");
-  scanf("%d",&n);
-  return n;
+#include <stdio.h>
+int input_side(){
+    int n;
+    printf("Enter a length of a triangle :\n ");
+    scanf("%d",&n);
+    return n;
 }
-int gcd(int a, int b)
-{
-  int i, x, t;
-  x=0;
-  if(a>b)
-  {
-    t=a;
-    a=b;
-    b=t;
-  }
-  if(b%a==0)
-    return a;
-  {
-    for(i=1;i<a/2;i++)
-      {
-        (a%i==0 && b%i == 0);
-        x=i;
-      }
-  }
-  return x;
+int check_scalene(int a,int b,int c){
+    if (a != b && b != c && a != c){
+        return 1;
+    }
+    else if ((a == b && b != c && a != c) || (a != b && b == c && a != c) || (a =! b && b != c && a == c)){
+        return 2;
+    }
+    else if (a == b && b == c && a == c){
+        return 3;
+    }
 }
-void output(int a, int b, int gcd)
-{
-  printf("%d is GCD of %d and %d\n", gcd, a, b);
+void output(int a,int c,int isscalene){
+    isscalene == 1 ? printf("The triangle is scalane") : (isscalene == 2 ? printf("The triangle is isosceles") : (isscalene == 3 ? printf("The triangle is equilateral") : printf("error")));
 }
-int main()
-{
-  int a, b, result;
-  a=input();
-  b=input();
-  result=gcd(a, b);
-  output(a, b, result);  
-  return 0;
+int main(){
+    int a,b,c,isscalene;
+    a = input_side();
+    b = input_side();
+    c = input_side();
+    isscalene = check_scalene(a,b,c);
+    output(a,c,isscalene);
+    return 0;
 }
